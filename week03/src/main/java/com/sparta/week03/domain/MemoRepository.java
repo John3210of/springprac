59e5ae3769a244a4a6463137763ae2e1f4596b99
john <john3210of@gmail.com>
2022-02-14 22:46:30 +0900

    강의3,4 완료

---
+++ week03/src/main/java/com/sparta/week03/domain/MemoRepository.java
@@ -0,0 +1,14 @@
+package com.sparta.week03.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+//public interface MemoRepository extends JpaRepository<Memo, Long> {
+//    List<Memo> findAllByOrderByModifiedAtDesc();
+//}
+public interface MemoRepository extends JpaRepository<Memo, Long> {
+    List<Memo> findAllByModifiedAtBetweenOrderByModifiedAtDesc(LocalDateTime start, LocalDateTime end);
+}
+//interface파일은 클래스에서 멤버 변수가 없는 메소드만 있는것
---
+++ week03/src/main/java/com/sparta/week03/service/MemoService.java
@@ -0,0 +1,25 @@
+package com.sparta.week03.service;
+
+import com.sparta.week03.domain.Memo;
+import com.sparta.week03.domain.MemoRepository;
+import com.sparta.week03.domain.MemoRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor    //생성자 자동으로 넣어줘
+@Service
+public class MemoService {
+
+    private final MemoRepository memoRepository;
+
+    @Transactional  //db에 반영이 되야한다를 알려주는것
+    public Long update(Long id, MemoRequestDto requestDto) {
+        Memo memo = memoRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("아이디가 존재하지 않습니다.")
+        );
+        memo.update(requestDto);
+        return memo.getId();
+    }
+}
