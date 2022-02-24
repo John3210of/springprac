005695bac75e2d5766d5097331bff68a78e7bb3f
john <john3210of@gmail.com>
2022-02-24 19:28:46 +0900

    gitignore

--- s/src/main/java/com/yohan/spring1/repository/LikesRepository.java
+++ s/src/main/java/com/yohan/spring1/repository/LikesRepository.java
@@ -3,7 +3,6 @@ package com.yohan.spring1.repository;
 import com.yohan.spring1.model.Likes;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
 
 
 public interface LikesRepository extends JpaRepository<Likes,Long> {
